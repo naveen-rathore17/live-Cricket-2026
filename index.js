4c13693d54de01248cbe5a90f1c9eeb618e3ba26
NaveenRathore5094 <rathorenaveen121@gmail.com>
2026-03-01 13:01:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/help", (req, res) => {
+  res.render("help");
+});
+app.get('/watch-live',(req,res)=>{
+  res.redirect('https://modderguy-star-sports-2hd-hindi.pages.dev/')
+})
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
