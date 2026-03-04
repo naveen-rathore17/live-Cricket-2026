b41f59fe7bce0849239ecb1716879acf41d2657e
NaveenRathore5094 <rathorenaveen121@gmail.com>
2026-03-04 11:32:43 +0530

    updated

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+const io = new Server(server);
 
 app.set("view engine", "ejs");
 
+// Live Users Counter
+let onlineUsers = 0;
+
+io.on("connection", (socket) => {
+  onlineUsers++;
+  console.log("User Connected:", onlineUsers);
+
+  io.emit("updateUsers", onlineUsers);
+
+  socket.on("disconnect", () => {
+    onlineUsers--;
+    console.log("User Disconnected:", onlineUsers);
+
+    io.emit("updateUsers", onlineUsers);
+  });
+});
+
+// Routes
 app.get("/", (req, res) => {
   res.render("home");
 });
+
 app.get("/help", (req, res) => {
   res.render("help");
 });
+
 app.get("/watch-live", (req, res) => {
   res.redirect("https://modderguy-star-sports-2hd-hindi.pages.dev/");
 });
-app.get('/legal',(req,res)=>{
-  res.render('info')
-})
 
-app.get('/squads',(req,res)=>{
-  res.render("squad")
-})
- 
+app.get("/legal", (req, res) => {
+  res.render("info");
+});
+
+app.get("/squads", (req, res) => {
+  res.render("squad");
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
-
-
-
-// Live link => https://allrounderid2.pages.dev/jjiotv/player?id=1985
